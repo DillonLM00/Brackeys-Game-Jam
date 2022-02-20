52ab2beee789ed07045d4fad109fb31d4f2d0201
MarhalmGH <93052627+MarhalmGH@users.noreply.github.com>
2022-02-20 18:36:22 +0100

    Flashlight cone + Toggle using mouse

---
+++ Brackeys-Game-Jam Unity/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlightLight;
+    private float batteryCharge = 100;
+    private float batteryUsePerSecond = 20; // how much charge gets used per second
+
+    private bool flashlightActive = false;
+    
+    private void Update()
+    {
+        flashlightLight.gameObject.SetActive(flashlightActive);
+
+        if (!Input.GetMouseButton(0))   // only continues code if the left mouse button is down
+        {
+            flashlightActive = false;
+            return;
+        }
+
+        if (batteryCharge <= 0) // only continues code if the battery has charges left
+        {
+            flashlightActive = false;
+            Debug.Log("Change Battery");
+            return;
+        }
+
+        batteryCharge -= batteryUsePerSecond * Time.deltaTime;    // uses charge per seconnd
+
+        flashlightActive = true;
+    }
+
+  
+
+
+}
