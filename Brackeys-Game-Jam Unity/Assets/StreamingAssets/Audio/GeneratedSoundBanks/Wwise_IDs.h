82b3e3a5cc9098cbc52ac40448089fa8b3c39b46
Nasinera <83073737+Nasinera@users.noreply.github.com>
2022-02-27 00:00:30 +0100

    Sounds Teil 1

---
+++ Brackeys-Game-Jam Unity/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CHANGE_BATTERY = 3020458995U;
+        static const AkUniqueID FLASHLIGHT_OFF = 1126494853U;
+        static const AkUniqueID FLASHLIGHT_ON = 2033671033U;
+        static const AkUniqueID PICKUP_BATTERY = 2557524463U;
+        static const AkUniqueID SPIDER_STEP = 3015918127U;
+        static const AkUniqueID TRASH_STEP = 1593284794U;
+        static const AkUniqueID ZIPPO_CLOSE = 2168122260U;
+        static const AkUniqueID ZIPPO_ON = 3894384011U;
+        static const AkUniqueID ZIPPO_OPEN = 3865570096U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_GAME = 951883595U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
