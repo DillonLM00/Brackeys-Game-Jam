55d4644ce2547aa2d0710f8a6ddfabaa233ff3bd
MasterJKraon <100077523+MasterJKraon@users.noreply.github.com>
2022-02-26 23:50:11 +0100

    SpiderSwap

--- Brackeys-Game-Jam Unity/Assets/Scripts/SpiderDeactivation.cs
+++ Brackeys-Game-Jam Unity/Assets/Scripts/SpiderDeactivation.cs
@@ -5,75 +5,39 @@ using UnityEngine;
 public class SpiderDeactivation : MonoBehaviour
 {
     public List<OpponentTransform> blackList;
-    public GameObject evilReplacer;
-    public GameObject harmlessReplacer;
-    private GameObject evil;
-    private GameObject harmless;
-
+    
     private void Start()
     {
-        evil = blackList[0].evilVersion;
-        harmless = blackList[0].harmlessVersion;
-
         if(!SpiderInformation.spidersActivated)
         {
             swapToReplacer();
         }
     }
 
-    private IEnumerator fixBug(bool cycle)
+    private void swapToReplacer()
     {
-        yield return new WaitForEndOfFrame();
-        for (int i = 0; i < blackList.Count; i++)
+        SpiderPartDeactivation spd;
+
+        for(int i = 0; i < blackList.Count; i++)
         {
-            blackList[i].transform.GetComponentInChildren<OpponentController>().setPatrouilleCycle(cycle);
-        }
-    }
-
-    public void swapToReplacer()
-    {
-        for (int i = 0; i < blackList.Count; i++)
-        {
-            Debug.Log("Spidy");
-            blackList[i].evilVersion = evilReplacer;
-            blackList[i].harmlessVersion = harmlessReplacer;
-
-            Transform pos = blackList[i].transform.GetChild(1).transform;
-            Destroy(blackList[i].transform.GetChild(1).gameObject);
-
-            if (blackList[i].isEvil)
+            spd = blackList[i].GetComponentInChildren<SpiderPartDeactivation>();
+            for (int k = 0; k < spd.parts.Count; k++)
             {
-                Instantiate(evilReplacer, pos.position, pos.rotation, blackList[i].transform);
-                StartCoroutine(fixBug(!blackList[i].transform.GetChild(1).gameObject.GetComponent<OpponentController>().getPatrouilleCycle()));
-            }
-            else
-            {
-                Instantiate(harmlessReplacer, pos.position, pos.rotation, blackList[i].transform);
+                spd.parts[k].SetActive(false);
             }
         }
     }
 
-    public void swapToNormal()
+    private void swapToNormal()
     {
+        SpiderPartDeactivation spd;
+
         for (int i = 0; i < blackList.Count; i++)
         {
-            Debug.Log("No Spidy");
-            blackList[i].evilVersion = evil;
-            blackList[i].harmlessVersion = harmless;
-
-            Transform pos = blackList[i].transform.GetChild(1).transform;
-            Destroy(blackList[i].transform.GetChild(1).gameObject);
-
-            
-
-            if (blackList[i].isEvil)
+            spd = blackList[i].GetComponentInChildren<SpiderPartDeactivation>();
+            for (int k = 0; k < spd.parts.Count; k++)
             {
-                Instantiate(evil, pos.position, pos.rotation, blackList[i].transform);
-                StartCoroutine(fixBug(!blackList[i].transform.GetChild(1).gameObject.GetComponent<OpponentController>().getPatrouilleCycle()));
-            }
-            else
-            {
-                Instantiate(harmless, pos.position, pos.rotation, blackList[i].transform);
+                spd.parts[k].SetActive(true);
             }
         }
     }
