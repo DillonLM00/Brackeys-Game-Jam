14ac43025c145ac2cfd99869a497432f2566d49d
MasterJKraon <100077523+MasterJKraon@users.noreply.github.com>
2022-02-27 01:39:07 +0100

    BatteryCollect

---
+++ Brackeys-Game-Jam Unity/Assets/Scripts/Player/CollectBattery.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectBattery : MonoBehaviour
+{
+    private Flashlight flashlight;
+
+    private void Start()
+    {
+        flashlight = FindObjectOfType<Flashlight>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            flashlight.exchangeBatteries++;
+    }
+}
